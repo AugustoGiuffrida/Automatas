77cf21c9182799a5ae2c9376eb3a4850eca26575
Eyudica <164026957+Eyudica@users.noreply.github.com>
2025-06-08 20:36:38 -0300

    Update tp5.py

--- tp5.py
+++ tp5.py
@@ -3,28 +3,33 @@
 import pandas as pd
 import re
 from datetime import datetime,timedelta,date
-#df=pd.read_csv("file.csv")
+df=pd.read_csv("file.csv")
 #usuarios=df['Usuario'].tolist()
 expresion_hora=re.compile(r"^([0-1][0-9]|[2][0-3])\:[0-5][0-9]\:[0-5][0-9]$") #19:46:08 |2[0-3]
 expresion_fecha=re.compile(r"^((2019|202[0-3])\-(0[1-9]|1[0-2])\-(0[1-9]|1[0-9]|2[0-9]|3[0-1]))$") ### 
-# # for i in range(len(usuarios)):
-
-# #     if usuarios[i]=="invitado-deca":
-# #        print(f"Usuario {usuarios[i]} conectado en {df['Session_Time'][i]} segundos con {df['Input_Octects'][i]} Octects de entrada y {df['Output_Octects'][i]} Octects de salida")
 
 
 
 def iterate_over_days(start_date, end_date):
+    diference_between_dates=[]
+
     delta = timedelta(days=1)
     while start_date <= end_date:
-        print(start_date.strftime("%Y-%m-%d"))
-        # # for i in range(len(usuarios)):
 
-# #     if usuarios[i]=="invitado-deca":
-# #        print(f"Usuario {usuarios[i]} conectado en {df['Session_Time'][i]} segundos con {df['Input_Octects'][i]} Octects de entrada y {df['Output_Octects'][i]} Octects de salida")
+        diference_between_dates.append(start_date.strftime("%Y-%m-%d"))
         start_date += delta
+    return diference_between_dates
 
-
+def get_users(diference_between_dates):
+    encontrados = 0
+    for index, row in df.iterrows(): #index es el numero de la fila y row el contenido
+        if row['Usuario'] == "invitado-deca" and (
+            row['Inicio_de_Conexión_Dia'] in diference_between_dates or 
+            row['FIN_de_Conexión_Dia'] in diference_between_dates):
+            encontrados += 1
+            print(f"- Usuario {row['Usuario']} conectado {row['Session_Time']} seg con {row['Input_Octects']} entrada y {row['Output_Octects']} salida")
+    print(f"\nTotal de conexiones encontradas: {encontrados}")
+    
 def check_regular(string,expresion):
     if expresion.search(string):
         return True
@@ -50,11 +55,13 @@ if __name__=="__main__":
             check_regular(fecha,expresion_fecha)
         for hora in hora:
             check_regular(hora,expresion_hora)
-        iterate_over_days(datetime.strptime(date[0], "%Y-%m-%d").date(),datetime.strptime(date[1], "%Y-%m-%d").date())    
+        diference_between_dates=iterate_over_days(datetime.strptime(date[0], "%Y-%m-%d").date(),datetime.strptime(date[1], "%Y-%m-%d").date())  
+        get_users(diference_between_dates)  
     except ValueError as e:
         print("Error:", e)
         exit()
         
         #2021-11-14 19:46:08
-        #2021-11-19 20:46:08
+        #2021-11-15 20:46:08
         
+
